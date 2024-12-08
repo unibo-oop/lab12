3255c621c4b3d13f55d7b65ebb93244f133a9860
pianinator <113355583+pianinator@users.noreply.github.com>
2024-12-08 10:46:25 +0000

    Deploying to solutions from @ APICe-at-DISI/exercises@180e6e106099f1e781fd579d786be0c81fd7c627 🚀

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,46 +1,49 @@
 package it.unibo.es1;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.*;
 
 public class LogicsImpl implements Logics {
-
+	
+	private final List<Integer> values;
+	
 	public LogicsImpl(int size) {
-		//TODO Auto-generated constructor stub
+		this.values = new ArrayList<>(Collections.nCopies(size,0));
 	}
-
+	
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return this.values.size();
 	}
 
 	@Override
 	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+		return Collections.unmodifiableList(values);
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		return values.stream().map(x -> x < values.size()).toList();
 	}
 
 	@Override
 	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+		int n = this.values.get(elem);
+		this.values.set(elem, ++n);
+		return n;
 	}
 
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		return this.values
+				.stream()
+				.map(String::valueOf)
+				.collect(Collectors.joining("|", "<<", ">>"));
 	}
 
 	@Override
 	public boolean toQuit() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+		return this.values.stream().allMatch(i -> i == this.values.get(0));
 	}
+	
 }
